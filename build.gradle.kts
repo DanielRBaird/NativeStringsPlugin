7bd10c19e36ac36ad719fd57b27f17a11bcbe177
Daniel Baird <danielrb11@gmail.com>
2020-07-26 18:21:20 -0400

    closer to working

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,18 +8,13 @@ data class LocaleFile(
     var name: String
 )
 
-open class NativeStringsPluginExtension {
-    var message: String? = null
-    var greeter: String? = null
-}
-
 open class GenerateInterfacesTask : DefaultTask() {
     val keySet: HashSet<String> = HashSet()
 
     var destination: Any? = null
     var input: Any? = null
 
-    fun getDestination(): File {
+    private fun getDestination(): File {
         return project.file(destination!!)
     }
 
@@ -43,11 +38,7 @@ open class GenerateInterfacesTask : DefaultTask() {
         // Grab the json text out of the strings file, and get the array.
         val json: ArrayList<Map<String, Any>> = JsonSlurper().parseText(input.readText()) as ArrayList<Map<String, Any>>
 
-        stringBuilder.appendln(
-            """
-            internal interface Strings {
-            """.trimIndent()
-        )
+        stringBuilder.appendln("""internal interface Strings {""")
 
         // This dictionary will contain a map of all of the ids, and the translation for the default language.
         for (stringObject in json) {
@@ -62,25 +53,41 @@ open class GenerateInterfacesTask : DefaultTask() {
     }
 }
 
-tasks.register<GenerateInterfacesTask>("generateInterface") {
+/*tasks.register<GenerateInterfacesTask>("generateInterface") {
     input = { project.extra["localizableStringsFile"]!! }
     destination = { project.extra["localizableStringsOutputPath"]!! }
-}
+}*/
 
-tasks.register("generate") {
+// This works to be able to run the generate thing.
+/*tasks.register("generate") {
     dependsOn("generateInterface")
-}
+}*/
 
 // Testing
 // extra["localizableStringsFile"] = "test/stringsDefault.json"
 // extra["localizableStringsOutputPath"] = "test"
 
-// TODO: How does the plugin project work with this?
+open class NativeStringsPluginExtension {
+    var destination: String? = null
+    var input: String? = null
+}
+
 class NativeStrings : Plugin<Project> {
     override fun apply(project: Project) {
-        //val extension = project.extensions.create<GreetingPluginExtension>("nativeStringsPlugin")
-        project.task("generateStrings") {
-            dependsOn.add("generateInterface")
+        val extension = project.extensions.create<NativeStringsPluginExtension>("nativeStringsPlugin")
+        project.tasks.register<GenerateInterfacesTask>("generateInterface") {
+            input = extension.input
+            destination = extension.destination
         }
     }
+}
+
+// Note: I think this is what would have to be done in another project:
+
+apply<NativeStrings>()
+
+// Configure the extension using a DSL block
+configure<NativeStringsPluginExtension> {
+    input = "test/stringsDefault.json"
+    destination = "test"
 }
--- test/Strings.kt
+++ test/Strings.kt
@@ -3,4 +3,5 @@ internal interface Strings {
     val example_key_2: String
     val description_add_comment: String
     val user_added_string: String
+    val home_page_sign_in: String
 }
